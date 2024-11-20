8c272b88f602066be78bf9992c2c4c1c1af8405e
Jacob Fu <jjacobfu@gmail.com>
2024-11-20 15:01:51 -0500

    added hero

---
+++ melodymatch/src/app/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+  <div className="hero-content text-center">
+    <div className="max-w-lg">
+      <h1 className="text-3xl font-bold">Introducing</h1>
+      <h1 className="text-7xl font-black">MelodyMatch</h1>
+      <p className="py-6"> Are you a musically match with your crush or significant other? Check now with MelodyMatch.
+      </p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default Hero
