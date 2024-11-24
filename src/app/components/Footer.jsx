34802bfda059dbb9f51cfb4a01e5b1ab73fe896e
Jacob Fu <jjacobfu@gmail.com>
2024-11-24 16:28:15 -0500

    fixed my git email

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
 <footer className="footer p-6 text-center text-xl justify-center ">
-    <h1>Created by Jacob Fu</h1>
+    <h1>Designed and Built by Jacob Fu</h1>
 </footer>  )
 }
 
